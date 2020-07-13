523ea4be11feab51535e0f051ede4a4632ad4602
SerBuv <sbuv62@mail.ru>
2020-07-13 15:14:18 +0300

    homework les1 task1

---
+++ homeworks/lesson_1/task_1.py
@@ -0,0 +1,25 @@
+# 1. Поработайте с переменными, создайте несколько, выведите на экран,
+# запросите у пользователя несколько чисел и строк и
+# сохраните в переменные, выведите на экран.
+
+my_var_1 = 54
+print(type(my_var_1))
+print(my_var_1)
+
+my_var_2 = 'Пример строкового типа данных'
+print(type(my_var_2))
+print(my_var_2)
+
+my_var_3 = True
+print(type(my_var_3))
+print(my_var_3)
+
+my_var_4 = 489.478
+print(type(my_var_4))
+print(my_var_4)
+
+enter_str = input('Введите свое имя: ')
+print(enter_str)
+
+enter_int = int(input('Веедите ваш возраст: '))
+print(enter_int)
